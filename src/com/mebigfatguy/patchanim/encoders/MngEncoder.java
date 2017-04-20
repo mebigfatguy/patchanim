487035a2855473138830d04cc81e2caab1900f4e
Dave Brosius <dbrosius@mebigfatguy.com>
2017-04-20 03:59:58 -0400

    use nio

--- src/com/mebigfatguy/patchanim/encoders/MngEncoder.java
+++ src/com/mebigfatguy/patchanim/encoders/MngEncoder.java
@@ -1,17 +1,17 @@
 /*
  * patchanim - A bezier surface patch color blend gif builder
  * Copyright (C) 2008 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -22,182 +22,186 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
 
 import com.mebigfatguy.patchanim.ExportType;
 
 /**
- * generates mng files by simply relying on the built in png encoder of ImageIO
- * and sewing together multiple chunks from the underlying pngs.
+ * generates mng files by simply relying on the built in png encoder of ImageIO and sewing together multiple chunks from the underlying pngs.
  */
-public class MngEncoder { 
-	private static final byte[] HEADER = new byte[] { -118, 77, 78, 71, 13, 10, 26, 10 };
-	private static final int IHDR = 0x49484452;
-	private static final int IDAT = 0x49444154;
-	private static final int IEND = 0x49454E44;
-	private static final int MHDR = 0x4D484452;
-	private static final int DEFI = 0x44454649;
-	private static final int SAVE = 0x53415645;
-	private static final int TERM = 0x5445524D;
-	private static final int SEEK = 0x5345454B;
-	private static final int LOOP = 0x4E4F4F50;
-	private static final int SHOW = 0x53484F59;
-	private static final int ENDL = 0x454E444E;
-	private static final int MEND = 0x4D454E44;
+public class MngEncoder {
+    private static final byte[] HEADER = new byte[] { -118, 77, 78, 71, 13, 10, 26, 10 };
+    private static final int IHDR = 0x49484452;
+    private static final int IDAT = 0x49444154;
+    private static final int IEND = 0x49454E44;
+    private static final int MHDR = 0x4D484452;
+    private static final int DEFI = 0x44454649;
+    private static final int SAVE = 0x53415645;
+    private static final int TERM = 0x5445524D;
+    private static final int SEEK = 0x5345454B;
+    private static final int LOOP = 0x4E4F4F50;
+    private static final int SHOW = 0x53484F59;
+    private static final int ENDL = 0x454E444E;
+    private static final int MEND = 0x4D454E44;
 
-	private DataOutputStream out = null;
-	private boolean started = false;
-	private boolean closeStream = false;
-	private boolean headerWritten = false;
-	private boolean repeatInfinite = false;
-	private int delay = 100;
-	private int frameCount = 1;
-	private int seqNum = 0;
-	
-	public void setDelay(int ms) {
-		delay = ms;
-	}
-	
-	public void setRepeat(boolean infinite) {
-		repeatInfinite = infinite;
-	}
-	
-	public void setNumFrames(int frames) {
-		frameCount = frames;
-	}
-	
-	public boolean start(OutputStream os) {
-		if (os == null) 
-			return false;
-		boolean ok = true;
-		closeStream = false;
-		out = new DataOutputStream(os);
-		try {
-			out.write(HEADER);
-		} catch (IOException e) {
-			ok = false;
-		}
-		return started = ok;
-	}
-	
-	public boolean start(String file) {
-		boolean ok = true;
-		try {
-			out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
-			ok = start(out);
-			closeStream = true;
-		} catch (IOException e) {
-			ok = false;
-		}
-		return started = ok;
-	}
-	
-	public boolean finish() {
-		if (!started) 
-			return false;
-		try {
-			Chunk saveChunk = new Chunk(0, SAVE);
-			saveChunk.calcCRC();
-			saveChunk.write(out);
-			
-			Chunk termChunk = new Chunk(10, TERM);
-			termChunk.injectShort(0, 0);
-			termChunk.injectInt(2, 0);
-			termChunk.injectInt(6, repeatInfinite ? 0x7FFFFFFF : 0);
-			
-			termChunk.calcCRC();
-			termChunk.write(out);
-			
-			Chunk seekChunk = new Chunk(0, SEEK);
-			seekChunk.calcCRC();
-			seekChunk.write(out);
-			
-			if (repeatInfinite) {
-				Chunk loopChunk = new Chunk(6, LOOP);
-				loopChunk.injectByte(0, 0);
-				loopChunk.injectInt(1, 0x7FFFFFFF);
-				loopChunk.injectByte(5, 6);
-				loopChunk.calcCRC();
-				loopChunk.write(out);
-			}
-			
-			Chunk showChunk = new Chunk(0, SHOW);
-			showChunk.calcCRC();
-			showChunk.write(out);
-			
-			if (repeatInfinite) {
-				Chunk endlChunk = new Chunk(1, ENDL);
-				endlChunk.injectByte(0, 0);
-				endlChunk.calcCRC();
-				endlChunk.write(out);
-			}
+    private DataOutputStream out = null;
+    private boolean started = false;
+    private boolean closeStream = false;
+    private boolean headerWritten = false;
+    private boolean repeatInfinite = false;
+    private int delay = 100;
+    private int frameCount = 1;
+    private int seqNum = 0;
 
-			Chunk iendChunk = new Chunk(0, MEND);
-			iendChunk.calcCRC();
-			iendChunk.write(out);
-			if (closeStream)
-				out.close();
-		} catch (IOException ioe) {
-		} finally {
-			out = null;
-			started = false;
-		}
-		
-		return true;
-	}
-	
-	public boolean addFrame(BufferedImage im) {
-		if ((im == null) || !started)
-			return false;
-		try {
-			PngStream pStrm;
-			{
-				ByteArrayOutputStream baos = new ByteArrayOutputStream(im.getHeight() * im.getWidth()); //a conservative estimate
-				ImageIO.write(im, ExportType.Pngs.getExtension(), baos);
-				pStrm = new PngStream(baos.toByteArray());
-				baos.reset();
-			}
-			Chunk chunk = pStrm.readNextChunk();
-			while (chunk != null) {
-				switch (chunk.type) {
-					case IHDR:
-						if (!headerWritten) {
-							Chunk mhdrChunk = new Chunk(28, MHDR);
-							mhdrChunk.injectInt(0, im.getWidth());
-							mhdrChunk.injectInt(4, im.getHeight());
-							mhdrChunk.injectInt(8, delay);
-							mhdrChunk.injectInt(12, 0);
-							mhdrChunk.injectInt(16, frameCount);
-							mhdrChunk.injectInt(20, frameCount);
-							mhdrChunk.injectInt(24, 583);
-							mhdrChunk.calcCRC();
-							mhdrChunk.write(out);
-							headerWritten = true;
-						}
-						Chunk defiChunk = new Chunk(4, DEFI);
-						defiChunk.injectInt(0, (seqNum++) << 16);
-						defiChunk.calcCRC();
-						defiChunk.write(out);
-						chunk.write(out);
-					break;
-					
-					case IDAT:
-						chunk.write(out);
-					break;
-					
-					case IEND:
-						chunk.write(out);
-					break;
-				}
-				chunk = pStrm.readNextChunk();
-			}
-			return true;
-		} catch (IOException ioe) {
-			return false;
-		}
-	}
+    public void setDelay(int ms) {
+        delay = ms;
+    }
+
+    public void setRepeat(boolean infinite) {
+        repeatInfinite = infinite;
+    }
+
+    public void setNumFrames(int frames) {
+        frameCount = frames;
+    }
+
+    public boolean start(OutputStream os) {
+        if (os == null) {
+            return false;
+        }
+        boolean ok = true;
+        closeStream = false;
+        out = new DataOutputStream(os);
+        try {
+            out.write(HEADER);
+        } catch (IOException e) {
+            ok = false;
+        }
+        return started = ok;
+    }
+
+    public boolean start(String file) {
+        boolean ok = true;
+        try {
+            out = new DataOutputStream(new BufferedOutputStream(Files.newOutputStream(Paths.get(file))));
+            ok = start(out);
+            closeStream = true;
+        } catch (IOException e) {
+            ok = false;
+        }
+        return started = ok;
+    }
+
+    public boolean finish() {
+        if (!started) {
+            return false;
+        }
+        try {
+            Chunk saveChunk = new Chunk(0, SAVE);
+            saveChunk.calcCRC();
+            saveChunk.write(out);
+
+            Chunk termChunk = new Chunk(10, TERM);
+            termChunk.injectShort(0, 0);
+            termChunk.injectInt(2, 0);
+            termChunk.injectInt(6, repeatInfinite ? 0x7FFFFFFF : 0);
+
+            termChunk.calcCRC();
+            termChunk.write(out);
+
+            Chunk seekChunk = new Chunk(0, SEEK);
+            seekChunk.calcCRC();
+            seekChunk.write(out);
+
+            if (repeatInfinite) {
+                Chunk loopChunk = new Chunk(6, LOOP);
+                loopChunk.injectByte(0, 0);
+                loopChunk.injectInt(1, 0x7FFFFFFF);
+                loopChunk.injectByte(5, 6);
+                loopChunk.calcCRC();
+                loopChunk.write(out);
+            }
+
+            Chunk showChunk = new Chunk(0, SHOW);
+            showChunk.calcCRC();
+            showChunk.write(out);
+
+            if (repeatInfinite) {
+                Chunk endlChunk = new Chunk(1, ENDL);
+                endlChunk.injectByte(0, 0);
+                endlChunk.calcCRC();
+                endlChunk.write(out);
+            }
+
+            Chunk iendChunk = new Chunk(0, MEND);
+            iendChunk.calcCRC();
+            iendChunk.write(out);
+            if (closeStream) {
+                out.close();
+            }
+        } catch (IOException ioe) {
+        } finally {
+            out = null;
+            started = false;
+        }
+
+        return true;
+    }
+
+    public boolean addFrame(BufferedImage im) {
+        if ((im == null) || !started) {
+            return false;
+        }
+        try {
+            PngStream pStrm;
+            {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream(im.getHeight() * im.getWidth()); // a conservative estimate
+                ImageIO.write(im, ExportType.Pngs.getExtension(), baos);
+                pStrm = new PngStream(baos.toByteArray());
+                baos.reset();
+            }
+            Chunk chunk = pStrm.readNextChunk();
+            while (chunk != null) {
+                switch (chunk.type) {
+                    case IHDR:
+                        if (!headerWritten) {
+                            Chunk mhdrChunk = new Chunk(28, MHDR);
+                            mhdrChunk.injectInt(0, im.getWidth());
+                            mhdrChunk.injectInt(4, im.getHeight());
+                            mhdrChunk.injectInt(8, delay);
+                            mhdrChunk.injectInt(12, 0);
+                            mhdrChunk.injectInt(16, frameCount);
+                            mhdrChunk.injectInt(20, frameCount);
+                            mhdrChunk.injectInt(24, 583);
+                            mhdrChunk.calcCRC();
+                            mhdrChunk.write(out);
+                            headerWritten = true;
+                        }
+                        Chunk defiChunk = new Chunk(4, DEFI);
+                        defiChunk.injectInt(0, (seqNum++) << 16);
+                        defiChunk.calcCRC();
+                        defiChunk.write(out);
+                        chunk.write(out);
+                    break;
+
+                    case IDAT:
+                        chunk.write(out);
+                    break;
+
+                    case IEND:
+                        chunk.write(out);
+                    break;
+                }
+                chunk = pStrm.readNextChunk();
+            }
+            return true;
+        } catch (IOException ioe) {
+            return false;
+        }
+    }
 }
--- src/com/mebigfatguy/patchanim/io/PatchAnimIO.java
+++ src/com/mebigfatguy/patchanim/io/PatchAnimIO.java
@@ -1,17 +1,17 @@
 /*
  * patchanim - A bezier surface patch color blend animation builder
  * Copyright (C) 2008 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -21,11 +21,11 @@ package com.mebigfatguy.patchanim.io;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -52,134 +52,133 @@ import com.mebigfatguy.patchanim.surface.Coordinate;
 import com.mebigfatguy.patchanim.surface.PatchCoords;
 
 public class PatchAnimIO {
-	private static final String PATCHANIMDOC_SCHEMA = "/com/mebigfatguy/patchanim/io/PatchAnimDoc.xsd";
-	
-	private PatchAnimIO() {
-		
-	}
+    private static final String PATCHANIMDOC_SCHEMA = "/com/mebigfatguy/patchanim/io/PatchAnimDoc.xsd";
 
-	public static void saveFile(File f, PatchAnimDocument document) throws IOException {
-		InputStream xslIs = null;
-		OutputStream xmlOs = null;
-		File temporary = File.createTempFile("patchanim", "paf", f.getParentFile());
-		try {
-			TransformerFactory tf = TransformerFactory.newInstance();
-			xslIs = new BufferedInputStream(PatchAnimIO.class.getResourceAsStream("/com/mebigfatguy/patchanim/io/PatchAnimDoc.xsl"));
-			Transformer t = tf.newTransformer(new StreamSource(xslIs));
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document d = db.newDocument();
-			xmlOs = new BufferedOutputStream(new FileOutputStream(temporary));
-			t.setParameter("doc", document);
-			t.transform(new DOMSource(d), new StreamResult(xmlOs));
-			document.setDirty(false);
-		} catch (IOException ioe) {
-			throw ioe;
-		} catch (Exception e) {
-			IOException ioe = new IOException("Failed saving document " + f.getPath());
-			ioe.initCause(e);
-			throw ioe;
-		} finally {
-			Closer.close(xslIs);
-			Closer.close(xmlOs);
-			f.delete();
-			if (!temporary.renameTo(f))
-				throw new IOException("Failed saving file " + f.getPath());
-		}
-	}
+    private PatchAnimIO() {
 
-	public static PatchAnimDocument loadFile(File f) throws IOException {
-		InputStream xmlIs = null;
-		
-		try {
-			XMLReader reader = XMLReaderFactory.createXMLReader();
-			PatchAnimDocContentHandler handler = new PatchAnimDocContentHandler();
-			reader.setContentHandler(handler);
-			xmlIs = new BufferedInputStream(new FileInputStream(f));
-			reader.setFeature("http://apache.org/xml/features/validation/schema", true);
-			reader.setFeature("http://xml.org/sax/features/validation", true);
-			reader.setProperty("http://apache.org/xml/properties/schema/external-noNamespaceSchemaLocation", PatchAnimDocument.class.getResource(PATCHANIMDOC_SCHEMA).toString());
+    }
 
-			reader.parse(new InputSource(xmlIs));
-			return handler.getDocument();
-		} catch (IOException ioe) {
-			throw ioe;
-		} catch (Exception e) {
-			IOException ioe = new IOException("Failed loading document " + f.getPath());
-			ioe.initCause(e);
-			throw ioe;
-		} finally {
-			Closer.close(xmlIs);
-		}
-	}
-	
-	static class PatchAnimDocContentHandler extends DefaultHandler {
-		private static final String SETTINGS = "Settings";
-		private static final String ORDER = "order";
-		private static final String ALPHA = "alpha";
-		private static final String WIDTH = "width";
-		private static final String HEIGHT = "height";
-		private static final String ANIMATIONTYPE = "animationType";
-		private static final String OUTOFBOUNDSCOLOR = "outOfBoundsColor";
-		private static final String TWEENCOUNT = "tweenCount";
-		private static final String TWEENSTYLE = "tweenStyle";
-		private static final String COMBINEDPATCH = "CombinedPatch";
-		private static final String PATCH = "Patch";
-		private static final String COLOR = "color";
-		private static final String COORDINATE = "Coordinate";
-		private static final String X = "x";
-		private static final String Y = "y";
-		
-		PatchAnimDocument doc = null;
+    public static void saveFile(File f, PatchAnimDocument document) throws IOException {
+        InputStream xslIs = null;
+        OutputStream xmlOs = null;
+        Path temporary = Files.createTempFile("patchanim", ".paf");
+        try {
+            TransformerFactory tf = TransformerFactory.newInstance();
+            xslIs = new BufferedInputStream(PatchAnimIO.class.getResourceAsStream("/com/mebigfatguy/patchanim/io/PatchAnimDoc.xsl"));
+            Transformer t = tf.newTransformer(new StreamSource(xslIs));
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document d = db.newDocument();
+            xmlOs = new BufferedOutputStream(Files.newOutputStream(temporary));
+            t.setParameter("doc", document);
+            t.transform(new DOMSource(d), new StreamResult(xmlOs));
+            document.setDirty(false);
+        } catch (IOException ioe) {
+            throw ioe;
+        } catch (Exception e) {
+            IOException ioe = new IOException("Failed saving document " + f.getPath());
+            ioe.initCause(e);
+            throw ioe;
+        } finally {
+            Closer.close(xslIs);
+            Closer.close(xmlOs);
+            f.delete();
+            Files.move(temporary, f.toPath());
+        }
+    }
 
-		private CombinedPatch cPatch = null;
-		private PatchCoords patchCoords = null;
-		private PatchColor patchColor = null;
-		private int coordIndex = 0;
-		
-		@Override
-		public void startElement(String uri, String localName, String qName, Attributes atts) {
-			if (SETTINGS.equals(localName)) {
-				int order = Integer.parseInt(atts.getValue(ORDER));
-				boolean alpha = Boolean.parseBoolean(atts.getValue(ALPHA));
-				doc = new PatchAnimDocument(order, alpha);
-				doc.getPatches().clear();
-				doc.setWidth(Integer.parseInt(atts.getValue(WIDTH)));
-				doc.setHeight(Integer.parseInt(atts.getValue(HEIGHT)));
-				doc.setAnimationType(Enum.<AnimationType>valueOf(AnimationType.class, atts.getValue(ANIMATIONTYPE)));
-				doc.setOutOfBoundsColor(Enum.<OutOfBoundsColor>valueOf(OutOfBoundsColor.class, atts.getValue(OUTOFBOUNDSCOLOR)));
-				doc.setTweenCount(Integer.parseInt(atts.getValue(TWEENCOUNT)));
-				doc.setTweenStyle(Enum.<TweenStyle>valueOf(TweenStyle.class, atts.getValue(TWEENSTYLE)));
-			} else if (COMBINEDPATCH.equals(localName)) {
-				cPatch = new CombinedPatch(doc.getOrder(), false);
-			} else if (PATCH.equals(localName)) {
-				patchCoords = new PatchCoords(doc.getOrder());
-				patchColor = Enum.<PatchColor>valueOf(PatchColor.class, atts.getValue(COLOR));
-			} else if (COORDINATE.equals(localName)) {
-				Coordinate c = new Coordinate(Double.parseDouble(atts.getValue(X)), 
-											  Double.parseDouble(atts.getValue(Y)), 
-											  Double.parseDouble(atts.getValue(COLOR)));
-				int order = doc.getOrder();
-				patchCoords.setCoordinate(coordIndex % order, coordIndex / order, c);
-				coordIndex++;
-			}
-		}
-		
-		@Override
-		public void endElement(String uri, String localName, String qName) {
-			if (COMBINEDPATCH.equals(localName)) {
-				doc.getPatches().add(cPatch);
-				cPatch = null;
-			} else if (PATCH.equals(localName)) {
-				cPatch.setPatch(patchColor, patchCoords);
-				patchColor = null;
-				patchCoords = null;
-				coordIndex = 0;
-			}
-		}
-		
-		public PatchAnimDocument getDocument() {
-			return doc;
-		}
-	}
+    public static PatchAnimDocument loadFile(File f) throws IOException {
+        InputStream xmlIs = null;
+
+        try {
+            XMLReader reader = XMLReaderFactory.createXMLReader();
+            PatchAnimDocContentHandler handler = new PatchAnimDocContentHandler();
+            reader.setContentHandler(handler);
+            xmlIs = new BufferedInputStream(Files.newInputStream(f.toPath()));
+            reader.setFeature("http://apache.org/xml/features/validation/schema", true);
+            reader.setFeature("http://xml.org/sax/features/validation", true);
+            reader.setProperty("http://apache.org/xml/properties/schema/external-noNamespaceSchemaLocation",
+                    PatchAnimDocument.class.getResource(PATCHANIMDOC_SCHEMA).toString());
+
+            reader.parse(new InputSource(xmlIs));
+            return handler.getDocument();
+        } catch (IOException ioe) {
+            throw ioe;
+        } catch (Exception e) {
+            IOException ioe = new IOException("Failed loading document " + f.getPath());
+            ioe.initCause(e);
+            throw ioe;
+        } finally {
+            Closer.close(xmlIs);
+        }
+    }
+
+    static class PatchAnimDocContentHandler extends DefaultHandler {
+        private static final String SETTINGS = "Settings";
+        private static final String ORDER = "order";
+        private static final String ALPHA = "alpha";
+        private static final String WIDTH = "width";
+        private static final String HEIGHT = "height";
+        private static final String ANIMATIONTYPE = "animationType";
+        private static final String OUTOFBOUNDSCOLOR = "outOfBoundsColor";
+        private static final String TWEENCOUNT = "tweenCount";
+        private static final String TWEENSTYLE = "tweenStyle";
+        private static final String COMBINEDPATCH = "CombinedPatch";
+        private static final String PATCH = "Patch";
+        private static final String COLOR = "color";
+        private static final String COORDINATE = "Coordinate";
+        private static final String X = "x";
+        private static final String Y = "y";
+
+        PatchAnimDocument doc = null;
+
+        private CombinedPatch cPatch = null;
+        private PatchCoords patchCoords = null;
+        private PatchColor patchColor = null;
+        private int coordIndex = 0;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts) {
+            if (SETTINGS.equals(localName)) {
+                int order = Integer.parseInt(atts.getValue(ORDER));
+                boolean alpha = Boolean.parseBoolean(atts.getValue(ALPHA));
+                doc = new PatchAnimDocument(order, alpha);
+                doc.getPatches().clear();
+                doc.setWidth(Integer.parseInt(atts.getValue(WIDTH)));
+                doc.setHeight(Integer.parseInt(atts.getValue(HEIGHT)));
+                doc.setAnimationType(Enum.<AnimationType> valueOf(AnimationType.class, atts.getValue(ANIMATIONTYPE)));
+                doc.setOutOfBoundsColor(Enum.<OutOfBoundsColor> valueOf(OutOfBoundsColor.class, atts.getValue(OUTOFBOUNDSCOLOR)));
+                doc.setTweenCount(Integer.parseInt(atts.getValue(TWEENCOUNT)));
+                doc.setTweenStyle(Enum.<TweenStyle> valueOf(TweenStyle.class, atts.getValue(TWEENSTYLE)));
+            } else if (COMBINEDPATCH.equals(localName)) {
+                cPatch = new CombinedPatch(doc.getOrder(), false);
+            } else if (PATCH.equals(localName)) {
+                patchCoords = new PatchCoords(doc.getOrder());
+                patchColor = Enum.<PatchColor> valueOf(PatchColor.class, atts.getValue(COLOR));
+            } else if (COORDINATE.equals(localName)) {
+                Coordinate c = new Coordinate(Double.parseDouble(atts.getValue(X)), Double.parseDouble(atts.getValue(Y)),
+                        Double.parseDouble(atts.getValue(COLOR)));
+                int order = doc.getOrder();
+                patchCoords.setCoordinate(coordIndex % order, coordIndex / order, c);
+                coordIndex++;
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) {
+            if (COMBINEDPATCH.equals(localName)) {
+                doc.getPatches().add(cPatch);
+                cPatch = null;
+            } else if (PATCH.equals(localName)) {
+                cPatch.setPatch(patchColor, patchCoords);
+                patchColor = null;
+                patchCoords = null;
+                coordIndex = 0;
+            }
+        }
+
+        public PatchAnimDocument getDocument() {
+            return doc;
+        }
+    }
 }
